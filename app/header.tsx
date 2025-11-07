14fdcd0cd44df34ade7d350ab49b28bac3f479f3
Franco <frfantusatti@protonmail.com>
2025-11-07 12:59:16 -0300

    fixed css modules

--- app/header.tsx
+++ app/header.tsx
@@ -1,27 +1,38 @@
 "use client";
 
 import Link from "next/link";
-import styles from "@/app/styles/Home.module.css";
 import { useStackApp, useUser } from "@stackframe/stack";
+import { Box, Heading, Button } from "@chakra-ui/react";
+import type { ReactNode } from "react";
 
-export function Header() {
+export function Header({ children }: { children?: ReactNode }) {
   const user = useUser();
   const app = useStackApp();
 
   return (
-    <header className={styles.header}>
-      <div>My Todo App</div>
-      {user ? (
-        <>
-          Hello {user.primaryEmail}
-          <Link href={app.urls.signOut}>Sign Out</Link>
-        </>
-      ) : (
-        <span>
-          <Link href={app.urls.signIn}>Sign In</Link> |{" "}
-          <Link href={app.urls.signUp}>Sign Up</Link>
-        </span>
-      )}
-    </header>
+    <Box background="tomato" width="100%" padding="4" color="white">
+      <div>
+        <div>
+          <Heading> App Notes </Heading>
+        </div>
+        {user ? (
+          <div className="flex justify-between items-center gap-4">
+            <div>{children}</div>
+            <Heading> Hello {user.primaryEmail} </Heading>
+            <Link href={app.urls.signOut}>Sign Out</Link>
+          </div>
+        ) : (
+          <div>
+            <Link href={app.urls.signIn}>
+              <Button>Sign in</Button>
+            </Link>{" "}
+            |
+            <Link href={app.urls.signUp}>
+              <Button>Sign Up</Button>
+            </Link>
+          </div>
+        )}
+      </div>
+    </Box>
   );
 }
